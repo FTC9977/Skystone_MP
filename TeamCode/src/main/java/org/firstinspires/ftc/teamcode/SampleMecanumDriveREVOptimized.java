38fc6501a343bb45eec6fbe91c918c0ef907928f
FTC9977 <eharwood6311@gmail.com>
2020-01-27 10:18:52 -0500

    Updated RoadRunner Drive Constants files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleMecanumDriveREVOptimized.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.roadrunner.control.PIDCoefficients;
+
+
+public class SampleMecanumDriveREVOptimized {
+}
---
+++ road-runner-master/core/src/main/kotlin/com/acmerobotics/roadrunner/trajectory/config/TrajectoryGroupConfig.kt
@@ -0,0 +1,36 @@
+package com.acmerobotics.roadrunner.trajectory.config
+
+import com.acmerobotics.roadrunner.trajectory.constraints.DriveConstraints
+import com.acmerobotics.roadrunner.trajectory.constraints.MecanumConstraints
+import com.acmerobotics.roadrunner.trajectory.constraints.TankConstraints
+import com.fasterxml.jackson.annotation.JsonIgnore
+
+class TrajectoryGroupConfig(
+    var constraints: DriveConstraints,
+    var distanceUnit: DistanceUnit,
+    var driveType: DriveType,
+    var trackWidth: Double?,
+    var wheelBase: Double?,
+    var lateralMultiplier: Double?
+) {
+    enum class DistanceUnit {
+        FOOT,
+        INCH,
+        METER,
+        CENTIMETER,
+        MILLIMETER
+    }
+
+    enum class DriveType {
+        GENERIC,
+        MECANUM,
+        TANK
+    }
+
+    val specificConstraints: DriveConstraints
+        @JsonIgnore get() = when (driveType) {
+            DriveType.GENERIC -> constraints
+            DriveType.MECANUM -> MecanumConstraints(constraints, trackWidth!!, wheelBase ?: trackWidth!!, lateralMultiplier!!)
+            DriveType.TANK -> TankConstraints(constraints, trackWidth!!)
+        }
+}
